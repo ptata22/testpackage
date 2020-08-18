dac186d2acb3bf2a289ba3359c1eb514d04d470f
ptata22 <rlanguage22@gmail.com>
2020-08-18 18:18:51 -0500

    update AE help file

--- R/ae.R
+++ R/ae.R
@@ -3,6 +3,16 @@
 #' @description A simple \code{tibble}
 #'
 #' @format A \code{tibble} with 33 column
+#' \describe{
+#' \item{studyid}{ Study Id }
+#' \item{domain}{ Domain}
+#' \item{usubjid}{ Unique subjid}
+#' \item{aeseq}{ AE Seq}
+#' \item{aespid}{ aespid}
+#' \item{aeterm}{ AE Unique TERM }
+#' \item{aellt}{ AE Low level term}
+#' \item{aelltcd}{ AE Low level Term CD}
+#'}
 
 "ae"
 
